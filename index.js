d11ceaf2a56a34f39b9d819049d851fb4e23aad5
hariom2308 <hom@babbel.com>
2018-05-24 15:40:55 +0200

    Refactored CSS & HTML

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-for(let i=0; i < 10; i++){
-  document.getElementById("button"+i).addEventListener("click", function( event ) {
-
-   document.getElementById("input").value += i ;
-
+for (let i = 0; i < 10; i++) {
+  document.getElementById("button" + i).addEventListener("click", function (event) {
+    document.getElementById("input").value += i ;
   })};
 
-for(let x = 1; x < 6; x++) {
-  document.getElementById("operator"+x).addEventListener("click", function( event ) {
+for (let x = 1; x < 6; x++) {
+  document.getElementById("operator" + x).addEventListener("click", function (event) {
     document.getElementById("input").value += this.value ;
-
   })};
 
-document.getElementById("clear").addEventListener("click", function( event ) {
-    document.getElementById("input").value=''
+document.getElementById("clear").addEventListener("click", function (event) {
+    document.getElementById("input").value = ''
 });
 
-document.getElementById("total").addEventListener("click", function( event ) {
+document.getElementById("total").addEventListener("click", function (event) {
     document.getElementById("input").value = eval(document.getElementById("input").value) ;
 });
