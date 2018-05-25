325d626cffdfad31901784bd2960eaeaa769ea06
Karen Hoyos <khoyos@babbel.com>
2018-05-25 15:33:01 +0200

    Removed eval and added calculator logic

--- index.js
+++ index.js
@@ -1,7 +1,68 @@
+
+function parseCalculationString(s) {
+
+var calculation = [],
+        current = '';
+    for (var i = 0, ch; ch = s.charAt(i); i++) {
+        if ('^*/+-'.indexOf(ch) > -1) {
+        	console.log("i =", i);
+        	console.log("ch is", ch);
+            if (current == '' && ch == '-') {
+                current = '-';
+            } else {
+                calculation.push(parseFloat(current), ch);
+                current = '';
+                console.log("current is", current)
+            }
+
+        } else {
+            current += s.charAt(i);
+        }
+    }
+////////////////////////////////////////////////////
+    if (current != '') {
+        calculation.push(parseFloat(current));
+    }
+    console.log("calculation is", calculation)
+    return calculation;
+
+}
+
+function calculate(calc) {
+    // --- Perform a calculation expressed as an array of operators and numbers
+    var operators = [
+               {'*': (a, b) => a * b, '/': (a, b) => a / b},
+               {'+': (a, b) => a + b, '-': (a, b) => a - b}
+               		],
+        newCalc = [],
+        currentOperator;
+    for (var i = 0; i < operators.length; i++) {
+        for (var j = 0; j < calc.length; j++) {
+            if (operators[i][calc[j]]) {
+                currentOperator = operators[i][calc[j]];
+            } else if (currentOperator) {
+                newCalc[newCalc.length - 1] = 
+                    currentOperator(newCalc[newCalc.length - 1], calc[j]);
+                currentOperator = null;
+            } else {
+                newCalc.push(calc[j]);
+            }
+            console.log(newCalc);
+        }
+        calc = newCalc;
+        newCalc = [];
+    }
+    if (calc.length > 1) {
+        console.log('Error: unable to resolve calculation');
+        return calc;
+    } else {
+        return calc[0];
+    }
+}
 for(let i=0; i < 10; i++){
   document.getElementById("button"+i).addEventListener("click", function( event ) {
 
-   document.getElementById("input").value += i ;
+   console.log(document.getElementById("input").value += i);
 
   })};
 
@@ -16,5 +77,5 @@ document.getElementById("clear").addEventListener("click", function( event ) {
 });
 
 document.getElementById("total").addEventListener("click", function( event ) {
-    document.getElementById("input").value = eval(document.getElementById("input").value) ;
+    document.getElementById("input").value = calculate(parseCalculationString(document.getElementById("input").value));
 });
